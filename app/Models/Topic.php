26832a81af00c83cea18fc1c26a2c8f147fa8617
chexiaoche <786401300@qq.com>
2018-04-18 10:30:05 +0000

    排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -1,10 +1,25 @@
 <?php
 
 namespace App\Models;
-
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function scopeWithOrder($query, $order)
+    {
+        // 不同的排序，使用不同的数据读取逻辑
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->recentReplied();
+                break;
+        }
+        // 预加载防止 N+1 问题
+        return $query->with('user', 'category');
+    }
 
     public function category()
     {
--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -4,6 +4,11 @@
 
 @section('content')
 
+<ul class="nav nav-pills">
+    <li class="{{ active_class( ! if_query('order', 'recent') ) }}"><a href="{{ Request::url() }}?order=default">最后回复</a></li>
+    <li class="{{ active_class(if_query('order', 'recent')) }}"><a href="{{ Request::url() }}?order=recent">最新发布</a></li>
+</ul>
+
 <div class="row">
     <div class="col-lg-9 col-md-9 topic-list">
         <div class="panel panel-default">
