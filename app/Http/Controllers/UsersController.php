797a13e1264f38eca91200171d59068d7a18ddf9
chexiaoche <786401300@qq.com>
2018-04-18 07:40:49 +0000

    update

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,16 +9,32 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+	public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     public function show(User $user)
     {
     	return view('users.show',compact('user'));
     }
     public function edit(User $user)
     {
+    	try {
+            $this->authorize ('update', $user);
+            return view ('users.edit', compact ('user'));
+        } catch (\Exception $e) {
+            abort(500, $e->getMessage());
+        }
     	return view('users.edit',compact('user'));
     }
     public function update(UserRequest $request,ImageUploadHandler $uploader,User $user)
     {
+    	try {
+            $this->authorize ('update', $user);
+        } catch (\Exception $e) {
+            abort(500, $e->getMessage());
+        }
     	$data = $request->all();
 
         if ($request->avatar) {
